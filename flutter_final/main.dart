092e26fde3194f29e2ab84a816c73b2fc9c2c387
mjankow4 <01169290@pw.edu.pl>
2024-01-22 09:56:14 +0100

    Upload New File

---
+++ flutter_final/main.dart
@@ -0,0 +1,23 @@
+// main.dart
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+import 'home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BarelyOwned App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+
